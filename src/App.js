caa70a38429b81013d22c6bbbbeac6012d76e87d
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-16 09:16:09 -0500

    created skills component, add css features border-bottom, padding to the skill list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
-
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <h1>Lauren's New Website</h1>
       <Home />
-    </div> 
+      <Skills />
+    </div>
   );
 }
 
