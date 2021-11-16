275e65c3eabe8c221497395099b521f4a7853228
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-16 09:31:58 -0500

    created projects compnents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <h1>Lauren's New Website</h1>
-      <Home />
-      <Skills />
+      <div className="intro">
+        <Home />
+        <Skills />
+      </div>
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      <h6>Projects</h6>
+    </div>
+  );
+};
+
+export default Projects;
