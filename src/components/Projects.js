190c391134bf85240af23556c2dc688498cb92c5
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-16 10:55:47 -0500

    added a secondary color: teal. Project bg changes to teal on hover. Font size for skills get larger on hover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,16 +7,19 @@ const Projects = () => {
       title: "On the Go",
       description:
         "This project keeps track of food trucks (registered) in your area. Truck owners register their food truck and when foodies search for food trucks in their area (by city and state), the food trucks are displayed.",
+      skillset: "Javascript, React, Redux,CSS, PostgreSQL DB",
     },
     {
       title: "Made Of",
       description:
         "This is a horoscope project with a 'Dear Abby' feature for anonymous advice.",
+      skillset: "Javascript, CSS, PostgreSQL DB",
     },
     {
       title: "Been There",
       description:
         "This project is meant to allow users to give others a review of their vacation. It can also be used to browse through reviews (by location) and pick a destination to try out.",
+      skillset: "Javascript, React, Redux, PostgreSQL DB",
     },
   ]);
 
@@ -27,7 +30,8 @@ const Projects = () => {
         {projects.map((p) => (
           <div className="project">
             <h5>{p.title} </h5>
-            {p.description}
+            {p.description} <br /> <br />
+            <i>Technologies used: {p.skillset}</i>
           </div>
         ))}
       </div>
