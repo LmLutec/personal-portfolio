58a98693e13c066fa2da48811ab425be4b7b096f
DevMcCoy <46851418+LmLutec@users.noreply.github.com>
2021-11-17 08:40:55 -0500

    css update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,25 @@ import Projects from "./components/Projects";
 function App() {
   return (
     <div className="App">
-      <h1>Lauren's New Website</h1>
+      <div className="header">
+        <h1>Lauren's New Website</h1>
+        <div className="contacts">
+          <a
+            href="https://www.linkedin.com/in/laurenmccoydev/"
+            className="contact"
+            target="_blank"
+          >
+            <img src="./images/linkedin-logo.png" alt="linkedin" />
+          </a>
+          <a
+            href="https://github.com/LmLutec"
+            className="contact"
+            target="_blank"
+          >
+            <img src="./images/github-logo.png" alt="Github" />
+          </a>
+        </div>
+      </div>
       <div className="intro">
         <Home />
         <Skills />
